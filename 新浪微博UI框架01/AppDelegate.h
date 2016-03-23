3bde1b5596def3e50a84808084cbe673e52dd6f4
labixiaoxin1019 <labixiaoxin1019@labixiaoxin1019demacbook-pro.local>
2016-03-23 20:50:52 +0800

    Initial Commit

---
+++ 新浪微博UI框架01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  新浪微博UI框架01
+//
+//  Created by labixiaoxin1019 on 16/3/23.
+//  Copyright © 2016年 labixiaoxin1019. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 新浪微博UI框架01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  新浪微博UI框架01
+//
+//  Created by labixiaoxin1019 on 16/3/23.
+//  Copyright © 2016年 labixiaoxin1019. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
