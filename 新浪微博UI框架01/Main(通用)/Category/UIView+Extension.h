f23a8732f9f7e495307701b879e928f5bde205b2
labixiaoxin1019 <labixiaoxin1019@labixiaoxin1019demacbook-pro.local>
2016-03-31 13:37:01 +0800

    UI界面

---
+++ 新浪微博UI框架01/Main(通用)/Category/UIView+Extension.h
@@ -0,0 +1,20 @@
+//
+//  UIView+Extension.h
+//  新浪微博UI框架01
+//
+//  Created by labixiaoxin1019 on 16/3/27.
+//  Copyright © 2016年 labixiaoxin1019. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView(Extension)
+
+@property (nonatomic,assign) CGFloat x;
+@property (nonatomic,assign) CGFloat y;
+@property (nonatomic,assign) CGFloat width;
+@property (nonatomic,assign) CGFloat height;
+@property (nonatomic,assign) CGSize size;
+@property (nonatomic,assign) CGPoint origin;
+
+@end
